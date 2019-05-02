e88495aa3506e571c9e1fedf93afbd2e9be20eb8
HernoDev <hernanronchese@gmail.com>
2019-05-02 19:33:29 -0300

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+;
+import { DatosService } from './datos.service'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -6,10 +8,10 @@ import { AppComponent } from './app.component';
 import { Subcomponente1Component } from './subcomponente/subcomponente1.component';
 import { Subcomponente2Component } from './subcomponente/subcomponente2.component';
 
-
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, Subcomponente1Component, Subcomponente2Component ],
+  providers: [ DatosService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
